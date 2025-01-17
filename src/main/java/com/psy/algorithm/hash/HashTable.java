d39f89db70adbac8bbf18e4213bc20a3d52fc9a1
Psy.C <abroadchen@163.com>
2025-01-17 23:19:55 +0800

    test

---
+++ src/main/java/com/psy/algorithm/hash/HashTable.java
@@ -0,0 +1,123 @@
+public class HashTable {
+
+    static class Entry {
+        int hash;
+        Object key;
+        Object value;
+        Entry next;
+
+        public Entry(int hash, Object key, Object value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    Entry[] table = new Entry[16];
+    int size = 0;
+    float loadFactor = 0.75f;
+    int threshold = (int) (table.length * loadFactor);
+
+    Object get(int hash, Object key) {
+        int idx = hash & (table.length - 1);
+        if (table[idx] == null) {
+            return null;
+        }
+        Entry p = table[idx];
+        while (p != null) {
+            if (p.key.equals(key)) {
+                return p.value;
+            }
+            p = p.next;
+        }
+        return null;
+    }
+
+    void put(int hash, Object key, Object value) {
+        int idx = hash & (table.length - 1);
+        if (table[idx] == null) {
+            table[idx] = new Entry(hash, key, value);
+        } else {
+            Entry p = table[idx];
+            while (true) {
+                if (p.key.equals(key)) {
+                    p.value = value;
+                    return;
+                }
+                if (p.next == null) {
+                    break;
+                }
+                p = p.next;
+            }
+            p.next = new Entry(hash, key, value);
+        }
+        size++;
+        if (size > threshold) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        Entry[] newTable = new Entry[table.length << 1];
+        for (int i = 0; i < table.length; i++) {
+            Entry p = table[i];
+            if (p != null) {
+                Entry a = null;
+                Entry b = null;
+                Entry aHead = null;
+                Entry bHead = null;
+                while (p != null) {
+                    if ((p.hash & table.length) == 0) {
+                        if (a != null) {
+                            a.next = p;
+                        } else {
+                            aHead = p;
+                        }
+                        a = p;
+                    } else {
+                        if (b != null) {
+                            b.next = p;
+                        } else {
+                            bHead = p;
+                        }
+                        b = p;
+                    }
+                    p = p.next;
+                }
+                if (a != null) {
+                    a.next = null;
+                    newTable[i] = aHead;
+                }
+                if (b != null) {
+                    b.next = null;
+                    newTable[i + table.length] = bHead;
+                }
+            }
+        }
+        table = newTable;
+        threshold = (int) (table.length * loadFactor);
+    }
+
+    Object remove(int hash, Object key) {
+        int idx = hash & (table.length - 1);
+        if (table[idx] == null) {
+            return null;
+        }
+        Entry p = table[idx];
+        Entry prev = null;
+        while (p != null) {
+            if (p.key.equals(key)) {
+                if (prev == null) {
+                    table[idx] = p.next;
+                } else {
+                    prev.next = p.next;
+                }
+                size--;
+                return p.value;
+            }
+            prev = p;
+            p = p.next;
+        }
+        return null;
+    }
+}
